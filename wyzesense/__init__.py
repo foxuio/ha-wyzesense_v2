c3e4658c564c28b4f1afe157eed25bcb3f6f89a9
foxuio <34701146+foxuio@users.noreply.github.com>
2025-05-13 02:52:54 -0500

    Delete wyzesense directory

--- wyzesense/__init__.py
+++
@@ -1,8 +0,0 @@
-"""WyzeSense integration for Home Assistant."""
-from homeassistant.core import HomeAssistant
-
-DOMAIN = "wyzesense"
-
-async def async_setup(hass: HomeAssistant, config: dict):
-    """Set up the WyzeSense integration."""
-    return True
